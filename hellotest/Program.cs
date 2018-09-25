428382dfd64ecaa77062927d028e0f0e968087ca
mackenzie.lee@intimetec.com <mackenzie.lee@intimetec.com>
2018-09-24 19:33:07 -0600

    Add project files.

---
+++ hellotest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellotest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
