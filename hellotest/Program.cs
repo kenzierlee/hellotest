82e9480338cf7762a0cbe7c4c05cd7b565a9b4da
mackenzie.lee@intimetec.com <mackenzie.lee@intimetec.com>
2018-09-25 10:00:35 -0600

    implemented guessing game

--- hellotest/Program.cs
+++ hellotest/Program.cs
@@ -6,7 +6,42 @@ namespace hellotest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			bool playing = true;
+
+			Random randomNumber = new Random();
+			int compAnswer = randomNumber.Next(1, 21);
+			int userNumber;
+
+			Console.WriteLine("Pick a number between 1 and 20");
+
+			while (playing)
+			{
+				string userAnswer = Console.ReadLine();
+				bool isNumber = int.TryParse(userAnswer, out userNumber);
+				checkResponse();
+			}
+
+			void checkResponse()
+			{
+				if (userNumber == compAnswer)
+				{
+					Console.WriteLine("Congratulations, you guessed right!");
+					playing = false;
+				}
+				else if (userNumber > compAnswer && userNumber <= 20)
+				{
+					Console.WriteLine("Your guess was too high, try again!");
+				}
+				else if (userNumber < compAnswer && userNumber >= 1)
+				{
+					Console.WriteLine("your guess was too low, try again!");
+				}
+				else
+				{
+					Console.WriteLine("Invalid Number");
+				}
+			}
+			
         }
     }
 }
